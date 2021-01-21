9c82a8e2dfc9956834c27199713fa2500e25b07a
svennif <33319198+svennif@users.noreply.github.com>
2021-01-21 02:19:55 +0100

    Search commit 1

---
+++ holidaze/src/Components/SearchForm.js
@@ -0,0 +1,11 @@
+import React, {useCallback} from "react";
+import BASE_URL from "../constants";
+import {Typeahead, Highlighter, Menu, MenuItem} from "react-bootstrap-typeahead";
+
+const URL = BASE_URL;
+
+function SearchForm() {
+  return <div></div>;
+}
+
+export default SearchForm;
