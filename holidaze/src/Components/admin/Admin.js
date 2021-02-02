9dd7cea7ef7b4955938953f4676ad5f07df61be0
svennif <33319198+svennif@users.noreply.github.com>
2021-02-02 14:02:20 +0100

    Admin update 2

--- holidaze/src/Components/admin/Admin.js
+++ holidaze/src/Components/admin/Admin.js
@@ -1,45 +1,11 @@
 import React from "react";
-
-function Admin() {
-  const adminEmail = "admin@holidaze.com";
-  const adminPassword = "Holidaze123";
-
-  localStorage.setItem("adminEmail", adminEmail);
-  localStorage.setItem("adminPassword", adminPassword);
-
-  function loginAdmin(adminEmail, adminPassword) {
-    const registeredAdmin = localStorage.getItem("adminEmail");
-    const registeredPassword = localStorage.getItem("adminPassword");
-
-    const validAdmin = adminEmail === registeredAdmin;
-    const validPassword = adminPassword === registeredPassword;
-
-    if (validAdmin && validPassword) {
-      console.log(123);
-      return `Welcome ${adminEmail}`;
-    } else if (!validAdmin) {
-      return `Wrong email`;
-    } else if (!validPassword) {
-      return `wrong password`;
-    }
-  }
-
-  return <div></div>;
-}
-
-export default Admin;
-
-/*import React from "react";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { FaRegArrowAltCircleRight } from "react-icons/fa";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import EnquiryModal from "../home/EnquiryModal";
-import { FaRegArrowAltCircleRight } from "react-icons/fa";
 
 const Schema = yup.object().shape({
   email: yup
@@ -53,76 +19,85 @@ const Schema = yup.object().shape({
     .matches(/[a-zA-Z]/, "Password can only contain Latin letters."),
 });
 
+const onSubmit = () => {
+  document.querySelector(".success").append("You have now logged in!");
+};
 
+const adminEmail = "admin@holidaze.com";
+const adminPassword = "Holidaze123";
+
+localStorage.setItem("adminEmail", adminEmail);
+localStorage.setItem("adminPassword", adminPassword);
 
 function Admin() {
+  function loginAdmin() {
+    const registeredAdmin = localStorage.getItem("adminEmail");
+    const registeredPassword = localStorage.getItem("adminPassword");
+    const emailValue = document.getElementById("adminEmail");
+    const passwordValue = document.getElementById("adminPassword");
+
+    if (
+      emailValue.value === registeredAdmin &&
+      passwordValue.value === registeredPassword
+    ) {
+      return console.log("Hello world!");
+    } else {
+      return console.log(errors.adminEmail?.message);
+    }
+  }
+
   const { register, handleSubmit, errors } = useForm({
     resolver: yupResolver(Schema),
   });
 
-  const onSubmit = () => {
-    document
-      .querySelector(".success")
-      .append("Success! Your message has been sent!");
-  };
-
   return (
-    <>
-      <Container className="col-xs-6">
-        <Row>
-          <Col sm className="admin__form">
-            <h1>Login</h1>
-            <Form onSubmit={handleSubmit(onSubmit)}>
-              <p className="success" style={{ color: "Green" }}></p>
-              <Form.Group>
-                <Form.Label>Email: </Form.Label>
-                <Form.Control
-                  id="adminEmail"
-                  size="lg"
-                  name="email"
-                  placeholder="Email..."
-                  ref={register({ required: true, minLength: 2 })}
-                />
-                <p>
-                  <strong>
-                    <i style={{ color: "Red" }}>{errors.email?.message}</i>
-                  </strong>
-                </p>
-              </Form.Group>
-              <Form.Group>
-                <Form.Label>Password: </Form.Label>
-                <Form.Control
-                  id="adminPassword"
-                  size="lg"
-                  type="password"
-                  name="password"
-                  placeholder="*******"
-                  ref={register}
-                />
-                <p>
-                  <strong>
-                    <i style={{ color: "Red" }}>{errors.password?.message}</i>
-                  </strong>
-                </p>
-              </Form.Group>
-
-              <div className="text-right">
-                <Button
-                  className="admin__button "
-                  variant="primary"
-                  type="submit"
-                  onClick={loginAdmin}
-                >
-                  Login <FaRegArrowAltCircleRight />
-                </Button>
-              </div>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-      <EnquiryModal />
-    </>
+    <Container>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <p className="success" style={{ color: "Blue" }}></p>
+        <Form.Group>
+          <Form.Label>Email: </Form.Label>
+          <Form.Control
+            id="adminEmail"
+            size="lg"
+            name="email"
+            placeholder="Email..."
+            ref={register({ required: true, minLength: 2 })}
+          />
+          <p>
+            <strong>
+              <i style={{ color: "Red" }}>{errors.adminEmail?.message}</i>
+            </strong>
+          </p>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Password: </Form.Label>
+          <Form.Control
+            id="adminPassword"
+            size="lg"
+            type="password"
+            name="password"
+            placeholder="*******"
+            ref={register}
+          />
+          <p>
+            <strong>
+              <i style={{ color: "Red" }}>{errors.adminPassword?.message}</i>
+            </strong>
+          </p>
+        </Form.Group>
+      </Form>
+      <div className="text-right">
+        <Button
+          className="admin__button "
+          variant="primary"
+          type="submit"
+          onClick={loginAdmin}
+        >
+          Login <FaRegArrowAltCircleRight />
+        </Button>
+      </div>
+    </Container>
   );
 }
 
-export default Admin; */
+export default Admin;
