ce541b96f18a9f04e5cf74858162ab9ca5ae5c48
svennif <33319198+svennif@users.noreply.github.com>
2021-02-12 12:05:53 +0100

    General commit 5

--- holidaze/src/Components/hotels/Hotels.js
+++ holidaze/src/Components/hotels/Hotels.js
@@ -1,36 +1,20 @@
 import React, { useState, useEffect } from "react";
-import Container from "react-bootstrap/Container";
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
 import { BASE_URL, FETCH_OPTIONS } from "../../Api";
 
-function Hotels() {
+function Hotels({ match }) {
   let [hotels, setHotels] = useState({});
 
   useEffect(() => {
     const hotelUrl = BASE_URL + "establishments";
-    fetch(hotelUrl, FETCH_OPTIONS)
+    fetch(hotelUrl + `/${match.params.id}`, FETCH_OPTIONS)
       .then((r) => r.json())
       .then((j) => setHotels(j))
       .catch((err) => console.log(err));
-  }, []);
+  }, [match.params.id]);
 
   return (
     <>
-      <Container>
-        <Card>
-          <Card.Header>{hotels.name}</Card.Header>
-          <Card.Body>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Title>Special title treatment</Card.Title>
-            <Card.Text>
-              With supporting text below as a natural lead-in to additional
-              content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-      </Container>
+      <p>{hotels.description}</p>
     </>
   );
 }
