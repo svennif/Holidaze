f6bd14a6c747280bab9700671ff1605194c25f7e
svennif <33319198+svennif@users.noreply.github.com>
2021-08-24 21:33:07 +0200

    Updates

--- src/Api.js
+++
@@ -1,8 +0,0 @@
-const KEY = "5ffdcdddbfee1036185d102f";
-export const BASE_URL = "https://us-central1-noroff-final-exam.cloudfunctions.net/api/v1/";
-export const headers = {
-    "Content-Type": "application/json",
-    key: KEY,
-};
-export const PATCH = "PATCH";
-export const DELETE = "DELETE";
--- src/Components/admin/Admin.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Login from "../auth/Login";
-
-function Admin() {
-  return <Login />;
-}
-
-export default Admin;
